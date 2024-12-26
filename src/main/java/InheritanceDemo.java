1c17fc2650bb7619c604e7458d3b22185b6c121d
unknown <bapcai1302@gmail.com>
2024-12-26 14:11:31 +0700

    create lesson6

---
+++ src/main/java/InheritanceDemo.java
@@ -0,0 +1,15 @@
+public class InheritanceDemo {
+    public static void main(String[] args) {
+        // Tính kế thừa
+        // Mục đích: Tái xử dụng code
+        // Từ khóa: extend
+        // Đơn kế thừa
+        Dog dog = new Dog("Muc", 3);
+        dog.run();
+
+        // this: Tham chiếu đến class đó
+        // super: Tham chiếu đến class cha
+    }
+
+
+}
